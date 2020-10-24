e0ca74800f35aee00cf6bf200986b35dd5cb9e4c
Jorge Oliveira <jorgeoliv32@gmail.com>
2020-10-23 23:01:49 -0300

    hey

---
+++ src/Title.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Title = () => {
+  return (
+    <div className="title">
+          <h2>Our Tours</h2>
+          <div ></div>
+    </div>
+  );
+};
+export default Title;
---
+++ src/Tour.js
@@ -0,0 +1,12 @@
+import React from 'react'
+const Tour = () => {
+    return <article>
+        <img src="" alt="" />
+        <footer>
+            <h3></h3>
+            <h3></h3>
+            <p></p>
+        </footer>
+    </article>
+}
+export default Tour
